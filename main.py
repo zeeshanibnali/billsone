15ab6917eb7f81722b634229feab36f608593868
Mohammed Zeeshan Ali <mzeeshanali2002@gmail.com>
2026-03-26 17:53:08 +0530

    feat: added fastapi and uvicorn

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
 def main():
-    print("Hello from example!")
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 
 
 if __name__ == "__main__":
     main()
-
